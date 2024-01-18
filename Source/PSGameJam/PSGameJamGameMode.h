f0f20f5f7bbc9bb19aeb0a461ca9ad0fda0be264
MElishaev <secret678@gmail.com>
2024-01-18 19:20:17 +0200

    Initial commit

---
+++ Source/PSGameJam/PSGameJamGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PSGameJamGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APSGameJamGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APSGameJamGameMode();
+};
+
+
+
