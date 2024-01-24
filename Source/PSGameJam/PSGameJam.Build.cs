25dd0cf1dc63cc154dca9e58de9f118da0fd57a6
MElishaev <secret678@gmail.com>
2024-01-24 10:27:20 +0200

    Removed CommonUtilities module dependency
    because for some reason it is dependent on UnrealEd, and you can't packaged with it into shipping mode

--- Source/PSGameJam/PSGameJam.Build.cs
+++ Source/PSGameJam/PSGameJam.Build.cs
@@ -8,6 +8,6 @@ public class PSGameJam : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "CommonUtilities" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 	}
 }
